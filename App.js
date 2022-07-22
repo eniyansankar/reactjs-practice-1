85b793d4cd5a11bea455f584f1c8176d493e725c
eniyansankar <109744356+eniyansankar@users.noreply.github.com>
2022-07-22 19:15:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div>
+        <div className='container'>
+        <h1 className='bg-info text-white display-2 text-centers ' >
+       Rediff money.com
+
+        </h1>
+        <h3 className='bg-info text-black display-8 text-centers ' >
+       best value site for your profitilo
+
+        </h3>
+
+        <p className='bg-warning lead fs-4 '> 
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+            crypto market tends to be fear bitcoin falls under  30k dollors
+
+
+        </p>
+
+        </div>
+        </div>
+        
+    );
+}
+
+export default App;
